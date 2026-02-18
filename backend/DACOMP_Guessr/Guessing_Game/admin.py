b4a03c0d2e98a0b5cc442ec9acfea3780ca44a2c
sgmaykon <ultra@Nokyam.(sgmaykon@estudante.ufscar.br)>
2026-02-18 00:10:16 -0300

    refactor: better seeding practices

--- backend/DACOMP_Guessr/Guessing_Game/admin.py
+++ backend/DACOMP_Guessr/Guessing_Game/admin.py
@@ -3,5 +3,8 @@ from .models import *
 
 admin.site.register(Session)
 admin.site.register(Player)
+admin.site.register(Round)
+admin.site.register(Location)
+admin.site.register(Guess)
 
 # Register your models here.
--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    \copy "Guessing_Game_location" FROM '/docker-entrypoint-initdb.d/Locatin_satellite.csv' DELIMITER ',' CSV HEADER;
+    \copy "Guessing_Game_location" FROM '/docker-entrypoint-initdb.d/Location_satellite.csv' DELIMITER ',' CSV HEADER;
 EOSQL
