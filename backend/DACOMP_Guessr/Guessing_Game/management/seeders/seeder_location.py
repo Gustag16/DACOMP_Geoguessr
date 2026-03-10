277f1fea2b6011e68de0f3ace476745c89a15f31
sgmaykon <ultra@Nokyam.(sgmaykon@estudante.ufscar.br)>
2026-03-09 23:51:56 -0300

    fix: csv location again

--- backend/DACOMP_Guessr/Guessing_Game/management/seeders/seeder_location.py
+++ backend/DACOMP_Guessr/Guessing_Game/management/seeders/seeder_location.py
@@ -1,19 +1,26 @@
 import csv
 from Guessing_Game.models import Location
-
+import os
+#id,image_url,latitude,longitude,place_name,category
 def run(stdout):
     if Location.objects.exists():
         stdout.write("Locations already seeded")
         return
 
-    with open("./Location_satellite.csv", newline='', encoding='utf-8') as csvfile:
+    base_dir = os.path.dirname(__file__)
+    csv_path = os.path.join(base_dir, "Location_satellite.csv")
+    with open(csv_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
 
         objs = [
             Location(
+                id=row["id"],
+                image_url=row["image_url"],
                 latitude=row["latitude"],
                 longitude=row["longitude"],
-                image=row["image"]
+                place_name=row["place_name"],
+                category=row["category"]
+
             )
             for row in reader
         ]
