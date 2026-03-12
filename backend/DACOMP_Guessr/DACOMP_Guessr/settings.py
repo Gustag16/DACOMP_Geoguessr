3df3af1afd879e2427cb258ef0fbaed3b7ff1ae1
sgmaykon <ultra@Nokyam.(sgmaykon@estudante.ufscar.br)>
2026-03-12 17:02:32 -0300

    feat: seeder files

--- backend/DACOMP_Guessr/DACOMP_Guessr/settings.py
+++ backend/DACOMP_Guessr/DACOMP_Guessr/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '0.0.0.0',
-    'barn-mortality-translator-hamilton.trycloudflare.com',
+    'encourage-assuming-mating-cope.trycloudflare.com',
     '.trycloudfare.com',
 ]
 
